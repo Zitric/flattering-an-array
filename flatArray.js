f2601dd3ead2b4de761379fc1305366b44cf26f5
Julian <julianpalomodeonis@ordersmadesimple.com>
2022-03-22 12:36:28 +0000

    Remove an extra falsy value check and allow not number types

--- flatArray.js
+++ flatArray.js
@@ -2,17 +2,20 @@ const iterateArray = (arrayToFlat, flattedArray) => {
   if (arrayToFlat.length === 0) return flattedArray;
 
   const element = arrayToFlat.shift();
-  if (typeof element === 'number') flattedArray.push(element);
-  else if (Array.isArray(element)) arrayToFlat = [...arrayToFlat, ...element];
+  // if element is an array concat the array elements to the arrayToFlat, flattering a flat
+  if (Array.isArray(element)) arrayToFlat = [...arrayToFlat, ...element];
+  // if the element is not nule add the element to the flattedArray
+  else if (element) flattedArray.push(element);
+
   iterateArray(arrayToFlat, flattedArray);
 };
 
 const flatArray = (arrayToFlat) => {
-  if (!arrayToFlat || !Array.isArray(arrayToFlat)) return [];
+  if (!Array.isArray(arrayToFlat)) return [];
 
   const flattedArray = [];
   iterateArray([...arrayToFlat], flattedArray);
-  return flattedArray.sort();
+  return flattedArray;
 };
 
 module.exports = flatArray;
