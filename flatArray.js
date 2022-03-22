91751559471fdbc779e9e3c6d0275dbf59874326
Julian <julianpalomodeonis@ordersmadesimple.com>
2022-03-22 08:39:32 +0000

    Refactor a conditional and fix bug in a posible case scenario

--- flatArray.js
+++ flatArray.js
@@ -2,21 +2,16 @@ const iterateArray = (arrayToFlat, flattedArray) => {
   if (arrayToFlat.length === 0) return flattedArray;
 
   const element = arrayToFlat.shift();
-  if (typeof element === 'number') {
-    flattedArray.push(element);
-    iterateArray(arrayToFlat, flattedArray);
-  } else if (Array.isArray(element)) {
-    iterateArray([...arrayToFlat, ...element], flattedArray);
-  } else {
-    iterateArray(arrayToFlat, flattedArray);
-  }
+  if (typeof element === 'number') flattedArray.push(element);
+  else if (Array.isArray(element)) arrayToFlat = [...arrayToFlat, ...element];
+  iterateArray(arrayToFlat, flattedArray);
 };
 
 const flatArray = (arrayToFlat) => {
   if (!arrayToFlat || !Array.isArray(arrayToFlat)) return [];
 
   const flattedArray = [];
-  iterateArray(arrayToFlat, flattedArray);
+  iterateArray([...arrayToFlat], flattedArray);
   return flattedArray.sort();
 };
 
